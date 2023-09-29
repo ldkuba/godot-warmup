78a26af44993d7696590bc7835107abc060870fc
Saman Miran <c@samanmiran.com>
2023-09-29 22:57:12 +0200

    coin anim

--- entities/coin.gd
+++ entities/coin.gd
@@ -3,13 +3,11 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	$AnimatedSprite2D.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
