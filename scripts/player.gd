2c106657b006c048b87a86b43fee9c7db9ba7511
Saman Miran <c@samanmiran.com>
2023-09-29 21:00:09 +0200

    Character animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,19 +7,37 @@ const JUMP_VELOCITY = -200.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var tex_standing = load("res://assets/sprites/Pixel Platformer/Characters/character_0000.png")
+var tex_standing_l = tex_standing.duplicate()
+var tex_jumping = load("res://assets/sprites/Pixel Platformer/Characters/character_0001.png")
+var tex_jumping_l = load("res://assets/sprites/Pixel Platformer/Characters/character_0001.png")
+
+var looking_left: bool
+var sprite_looking_left: bool
 
 func _physics_process(delta):
+	if looking_left:
+		$Sprite.scale = Vector2(1, 1)
+	else:
+		$Sprite.scale = Vector2(-1, 1)
+			
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	else:
+		$Sprite.texture = tex_standing
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("player_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		$Sprite.texture = tex_jumping
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("player_left", "player_right")
+	if direction != 0:
+		looking_left = direction < 0
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
